0781a47bbb0b87061926f18e0f30c2a0c374b946
Gerald Koh Jia Le <geraldkohjiale@gmail.com>
2026-02-14 20:32:56 +0800

    Add pencil icon

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1512,
     height: 982,
+    icon: path.join(__dirname, "excalidraw-app/icon.png"),
   });
 
   win.loadFile("./excalidraw-app/dist/index.html");
